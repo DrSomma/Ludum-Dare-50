c97936b5d9e3bb934beb7105f034388520f10c18
teebaron <sgraba@web.de>
2022-04-03 01:10:34 +0200

    cody

--- Ludum-Dare-50/Assets/Scripts/Platforms/MovingPlatform.cs
+++ Ludum-Dare-50/Assets/Scripts/Platforms/MovingPlatform.cs
@@ -1,36 +1,35 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
-using UnityEngine.Serialization;
 
-public class MovingPlatform : MonoBehaviour
+namespace Platforms
 {
-    [SerializeField]
-    private float speed;
+    public class MovingPlatform : MonoBehaviour
+    {
+        [SerializeField]
+        private float speed;
 
-    [SerializeField]
-    private Vector2 endPosRelativ;
-    private Vector3 EndPos => new Vector3(x: transform.position.x + endPosRelativ.x,y: transform.position.y + endPosRelativ.y);
+        [SerializeField]
+        private Vector2 endPosRelativ;
+        private Vector3 EndPos => new Vector3(x: transform.position.x + endPosRelativ.x,y: transform.position.y + endPosRelativ.y);
     
-    private void Start()
-    {
-        transform.DOMove(endValue: EndPos, duration: speed).SetEase(Ease.InOutSine).SetSpeedBased().SetLoops(loops: -1,loopType: LoopType.Yoyo);
-    }
+        private void Start()
+        {
+            transform.DOMove(endValue: EndPos, duration: speed).SetEase(Ease.InOutSine).SetSpeedBased().SetLoops(loops: -1,loopType: LoopType.Yoyo);
+        }
 
-    private void OnCollisionEnter2D(Collision2D col)
-    {
-        col.transform.SetParent(transform);
-    }
+        private void OnCollisionEnter2D(Collision2D col)
+        {
+            col.transform.SetParent(transform);
+        }
 
-    private void OnCollisionExit2D(Collision2D col)
-    {
-        col.transform.SetParent(null);
-    }
+        private void OnCollisionExit2D(Collision2D col)
+        {
+            col.transform.SetParent(null);
+        }
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.DrawSphere(center: EndPos,radius: 0.4f);
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawSphere(center: EndPos,radius: 0.4f);
+        }
     }
 }
