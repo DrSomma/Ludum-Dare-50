ea8add63170d1b830f190472becc3fb26d36e9b5
teebaron <sgraba@web.de>
2022-04-03 01:01:35 +0200

    Breaking

--- Ludum-Dare-50/Assets/Scripts/Platforms/BreakingPlatform.cs
+++ Ludum-Dare-50/Assets/Scripts/Platforms/BreakingPlatform.cs
@@ -1,18 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class BreakingPlatform : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private float speed = 1f;
+    
+    [SerializeField]
+    private float strength = 20f;
+    [SerializeField]
+    private int loopCount = 3;
+    
+    private bool _isBreaking;
+    
+    private void OnCollisionEnter2D(Collision2D col)
     {
-        
+        if (!col.gameObject.CompareTag("Player"))
+        {
+            return;
+        }
+
+        Break();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        if(Input.GetKeyDown(KeyCode.G))
+            Break();
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            _isBreaking = false;
+            Debug.Log("Kill: " + transform.DOKill(true));
+            transform.rotation = Quaternion.identity;
+        }
+    }
+
+    private void Break()
+    {
+        if (_isBreaking)
+            return;
+        _isBreaking = true;
         
+        var sq = DOTween.Sequence();
+        sq.Append(transform.DORotate(Vector3.forward * strength, speed, RotateMode.FastBeyond360));
+        sq.Append(transform.DORotate(Vector3.back * strength, speed, RotateMode.FastBeyond360));
+        sq.SetLoops(loopCount, LoopType.Yoyo);
+        sq.OnComplete(DestroyMe);
+    }
+
+    private void DestroyMe()
+    {
+        Destroy(gameObject);
     }
 }
