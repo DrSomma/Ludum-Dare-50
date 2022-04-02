7b5991ef87598db7880e890510e10c8395ffaa50
teebaron <sgraba@web.de>
2022-04-03 01:10:18 +0200

    cody

--- Ludum-Dare-50/Assets/Scripts/Platforms/BreakingPlatform.cs
+++ Ludum-Dare-50/Assets/Scripts/Platforms/BreakingPlatform.cs
@@ -1,62 +1,62 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
 
-public class BreakingPlatform : MonoBehaviour
+namespace Platforms
 {
-    [SerializeField]
-    private float speed = 1f;
-    
-    [SerializeField]
-    private float strength = 20f;
-    [SerializeField]
-    private int loopCount = 3;
-    
-    private bool _isBreaking;
-    
-    private void OnCollisionEnter2D(Collision2D col)
+    public class BreakingPlatform : MonoBehaviour
     {
-        if (!col.gameObject.CompareTag("Player"))
+        [SerializeField]
+        private float speed = 1f;
+    
+        [SerializeField]
+        private float strength = 20f;
+        [SerializeField]
+        private int loopCount = 3;
+    
+        private bool _isBreaking;
+    
+        private void OnCollisionEnter2D(Collision2D col)
         {
-            return;
+            if (!col.gameObject.CompareTag("Player"))
+            {
+                return;
+            }
+
+            Break();
         }
 
-        Break();
-    }
+        //Just for debug!
+        // private void Update()
+        // {
+        //     if(Input.GetKeyDown(KeyCode.G))
+        //         Break();
+        //     if (Input.GetKeyDown(KeyCode.H))
+        //     {
+        //         _isBreaking = false;
+        //         Debug.Log("Kill: " + transform.DOKill(true));
+        //         transform.rotation = Quaternion.identity;
+        //     }
+        // }
 
-    //Just for debug!
-    // private void Update()
-    // {
-    //     if(Input.GetKeyDown(KeyCode.G))
-    //         Break();
-    //     if (Input.GetKeyDown(KeyCode.H))
-    //     {
-    //         _isBreaking = false;
-    //         Debug.Log("Kill: " + transform.DOKill(true));
-    //         transform.rotation = Quaternion.identity;
-    //     }
-    // }
-
-    private void Break()
-    {
-        if (_isBreaking)
+        private void Break()
         {
-            return;
-        }
+            if (_isBreaking)
+            {
+                return;
+            }
 
-        _isBreaking = true;
+            _isBreaking = true;
         
-        Sequence sq = DOTween.Sequence();
-        sq.Append(transform.DORotate(endValue: Vector3.forward * strength, duration: speed, mode: RotateMode.FastBeyond360));
-        sq.Append(transform.DORotate(endValue: Vector3.back * strength, duration: speed, mode: RotateMode.FastBeyond360));
-        sq.SetLoops(loops: loopCount, loopType: LoopType.Yoyo);
-        sq.OnComplete(DestroyMe);
-    }
+            Sequence sq = DOTween.Sequence();
+            sq.Append(transform.DORotate(endValue: Vector3.forward * strength, duration: speed, mode: RotateMode.FastBeyond360));
+            sq.Append(transform.DORotate(endValue: Vector3.back * strength, duration: speed, mode: RotateMode.FastBeyond360));
+            sq.SetLoops(loops: loopCount, loopType: LoopType.Yoyo);
+            sq.OnComplete(DestroyMe);
+        }
 
-    private void DestroyMe()
-    {
-        Destroy(gameObject);
+        private void DestroyMe()
+        {
+            Destroy(gameObject);
+        }
     }
 }
