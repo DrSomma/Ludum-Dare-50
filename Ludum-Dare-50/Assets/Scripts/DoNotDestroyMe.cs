ffe931e29562e73c7dae1e58959619db7d4b643a
teebaron <sgraba@web.de>
2022-04-03 19:44:12 +0200

    fix DoNotDestroy

--- Ludum-Dare-50/Assets/Scripts/DoNotDestroyMe.cs
+++ Ludum-Dare-50/Assets/Scripts/DoNotDestroyMe.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class DoNotDestroyMe : MonoBehaviour
 {
+    private static bool _isInSceen;
+    
     void Start()
     {
+        if(_isInSceen)
+            Destroy(gameObject);
+        _isInSceen = true;
+        
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(true);
+        }
         DontDestroyOnLoad(gameObject);
     }
 }
